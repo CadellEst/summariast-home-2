c5bb1e1f7c23be97651bd4c639737dbd723f01dd
CadellEst <139273238+CadellEst@users.noreply.github.com>
2024-11-27 10:21:39 +0000

    update

---
+++ app/player/Player.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react'
+
+export default function player() {
+    useEffect(() => {
+        window.playSong = playsong
+    })
+  return (
+    <div>player</div>
+  )
+}
